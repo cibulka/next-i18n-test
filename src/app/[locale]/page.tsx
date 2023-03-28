9438091ab8d2b2c0330c0d577b1a2e4236e7d011
Petr Cibulka <cibulka.me@gmail.com>
2023-03-28 15:47:13 +0200

    fix: recursive import

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import { notFound } from 'next/navigation';
 
 import { MdxReader } from '@/components/mdx-reader/MdxReader';
 import { allPages } from '@/constants/content';
-import { Page, getDocument } from '@/server/getDocument';
+import { getDocument } from '@/server/getDocument';
 import { Locale } from '@/types';
 
 export default async function Page(props: { params: { locale: Locale } }) {
